b11635d43263e532e18bcffffb4036e345641faf
Maksat1990 <azhigaliyev_maksat@mail.ru>
2022-08-31 02:54:34 +0600

    Did!

---
+++ Task02/Program.cs
@@ -0,0 +1,22 @@
+﻿// Задача 2: Напишите программу, которая на вход принимает два числа и выдаёт, какое число большее, а какое меньшее.
+
+// a = 5; b = 7 -> max = 7
+// a = 2 b = 10 -> max = 10
+// a = -9 b = -3 -> max = -3
+
+Console.Write("Enter the first number: ");
+int a = Convert.ToInt32(Console.ReadLine());
+Console.Write("Enter the second number: ");
+int b = Convert.ToInt32(Console.ReadLine());
+
+if (a > b)
+{
+    Console.WriteLine($"Max number is: {a}");
+}
+else if (a < b)
+{
+    Console.WriteLine($"Max number is: {b}");
+} else 
+{
+    Console.WriteLine("Numbers are equal");
+}
---
+++ Task06/Program.cs
@@ -0,0 +1,17 @@
+﻿// Напишите программу, которая на вход принимает число и выдаёт, является ли число чётным (делится ли оно на два без остатка).
+
+// 4 -> да
+// -3 -> нет
+// 7 -> нет
+
+Console.Write("Please, enter the integer number: ");
+int num = Convert.ToInt32(Console.ReadLine());
+
+if (num % 2 == 0)
+{
+    Console.WriteLine($"{num} is even");
+}
+else 
+{
+    Console.WriteLine($"{num} is odd");
+}
